1d0ef0c7102be8d5abda2c735eb78715529e3712
Ayaan-Jutt <ayaanjutt416@gmail.com>
2023-02-23 12:11:16 -0700

    Added some QR stuff

---
+++ QRGo/app/src/main/java/com/example/qrgo/QR.java
@@ -0,0 +1,60 @@
+package com.example.qrgo;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class QR {
+    private String id;
+    private String scannedBy;
+    private Integer score;
+    private String face;
+    private Integer scannedAmnt;
+    private FirebaseFirestore db;
+
+    public QR(String id, String scannedBy, Integer score, String face) {
+        this.id = id;
+        this.scannedBy = scannedBy;
+        this.score = score;
+        this.face = face;
+        this.scannedAmnt = 1;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getScannedBy() {
+        return scannedBy;
+    }
+
+    public void setScannedBy(String scannedBy) {
+        this.scannedBy = scannedBy;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getFace() {
+        return face;
+    }
+
+    public void setFace(String face) {
+        this.face = face;
+    }
+
+    public Integer getScannedAmnt() {
+        return scannedAmnt;
+    }
+
+    public void setScannedAmnt(Integer scannedAmnt) {
+        this.scannedAmnt = scannedAmnt;
+    }
+}
---
+++ QRGo/app/src/main/java/com/example/qrgo/QRReader.java
@@ -0,0 +1,117 @@
+package com.example.qrgo;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class QRReader {
+    private Dictionary<String, Integer> dict = new Hashtable<>();
+    private String[][] nameChoices = {
+            {"The ", "A "},
+            {"Greatly ", "Mundanely "},
+            {"Unique ", "Average "},
+            {"Joy", "Strange"},
+            {"Pig ", "Sir "},
+            {"Bildalf","Gando"}
+    };
+    private String[][] imageChoice = {
+            {"\n| ~    ~ |", "\n| ==  == |" },
+            {"\n| 0    0 |", "\n| ^    ^ |"},
+            {"\n|  (--)  |", "\n|   ||   |\n|   LL   |"},
+            {"\n|  ====  |", "\n|        |"},
+            {"\n| '    ' |\n|  ```   |", "\n|   ---  |\n| '    ' |"},
+            {"\n|   <<   |\n", "\n|        |\n"}
+    };
+    private String content;
+    private byte[] bytes;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    public String hashedContent;
+    public Integer score;
+    public String name = "";
+    public String face = "";
+
+    public void readQR(){
+
+        FirebaseFirestore db;
+        createHash();
+        calcScore();
+        createFaceAndName();
+
+        
+
+    }
+    private void createHash(){
+        MessageDigest messageDigest;
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        bytes = messageDigest.digest(content.getBytes());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            sb.append(String.format("%02x", bytes[i]));
+        }
+        hashedContent = sb.toString();
+    }
+
+    private void calcScore(){
+        String repeatedChars;
+        Double scoreD;
+        Character character;
+        Pattern p = Pattern.compile("(\\w)\\1+");
+        Matcher m = p.matcher(hashedContent);
+        fillDictionary();
+
+        while (m.find()) {
+            repeatedChars = m.group();
+            character = repeatedChars.charAt(0);
+
+            scoreD = Math.pow( Double.valueOf(dict.get(character.toString())), Double.valueOf(repeatedChars.length() - 1));
+            score += scoreD.intValue();
+        }
+
+
+    }
+
+    private void fillDictionary(){
+        dict.put("0", 20);
+        Integer i;
+        for (i = 1; i < 10; i++){
+            dict.put(i.toString(), i);
+        }
+
+        for (Character c = 'a'; c <= 'f'; c++){
+            dict.put(c.toString(), i);
+            i++;
+        }
+    }
+
+
+    private void createFaceAndName(){
+        face = " --------";
+        Character bit;
+        String evenChars = "02468ace";
+        for (int i = 0; i < 6; i++){
+            bit = hashedContent.charAt(i);
+            if (evenChars.contains(bit.toString()) ){
+                face += imageChoice[i][0];
+                name += nameChoices[i][0];
+            } else {
+                face += imageChoice[i][1];
+                name += nameChoices[i][1];
+            }
+            face += " --------";
+        }
+
+    }
+}
+
+
