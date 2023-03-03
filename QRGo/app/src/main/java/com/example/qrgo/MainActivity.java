1c15cd4dab534fdfedd8ac74c18ef9fb4966b163
triaksh25 <100975695+triaksh25@users.noreply.github.com>
2023-03-02 17:04:22 -0700

    FirstFix

--- QRGo/app/src/main/java/com/example/qrgo/MainActivity.java
+++ QRGo/app/src/main/java/com/example/qrgo/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.qrgo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
