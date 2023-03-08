2acf6d35732b65581aaff4394da6b2ce23bf924b
FaiyadRahman <faiyad.r@yahoo.com>
2023-03-07 21:56:03 -0700

    implemented qr scanning
    got the functionality working for opening the camera and scanning the QR code

--- QRGo/app/src/main/java/com/example/qrgo/MainActivity.java
+++ QRGo/app/src/main/java/com/example/qrgo/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.qrgo;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button scanQRButton;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         scanQRButton = findViewById(R.id.scanQRButton);
+         textView = findViewById(R.id.textView);
+         scanQRButton.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 scanQRCode();
+             }
+         });
     }
+
+    private void scanQRCode() {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("prompt test");
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureActivity.class);
+        barLauncher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result -> {
+       if (result.getContents() != null) {
+           AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+           builder.setTitle("Result");
+           builder.setMessage(result.getContents());
+           textView.setText(result.getContents());
+           builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+               @Override
+               public void onClick(DialogInterface dialogInterface, int i) {
+                   dialogInterface.dismiss();
+               }
+           }).show();
+       }
+    });
 }
