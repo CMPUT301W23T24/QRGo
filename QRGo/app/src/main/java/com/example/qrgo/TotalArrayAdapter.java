1d241883709bdac50e8a9c92751b456ff6be0391
amn-xyz <amaan2@ualberta.ca>
2023-03-25 16:59:10 -0600

    Some XML stuff

---
+++ QRGo/app/src/main/java/com/example/qrgo/TotalArrayAdapter.java
@@ -0,0 +1,65 @@
+package com.example.qrgo;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class TotalArrayAdapter extends ArrayAdapter<String> {
+
+    /**
+     * Displays the content into the List view
+     * @param context
+     * @param users
+     */
+    public TotalArrayAdapter(Context context, ArrayList<String> users){
+        super(context, 0, users);
+    }
+
+    /**
+     *  gets the view of the branch
+     * @param position
+     * @param convertView
+     * @param parent
+     * @return the view of the current page
+     */
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup
+            parent) {
+        View view;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.total_score_content,
+                    parent, false);
+        } else {
+            view = convertView;
+        }
+
+        String userId = getItem(position);
+        User user = new User(userId);
+        user.getValuesFromDb(userId, new User.OnUserLoadedListener() {
+            @Override
+            public void onUserLoaded(User user) {
+                TextView loaded_user = view.findViewById(R.id.user_name);
+                loaded_user.setText(user.getUserName());
+
+                TextView total_score = view.findViewById(R.id.total_score);
+                total_score.setText(user.getScore().toString());
+            }
+        });
+
+        Log.d("userId", userId);
+        Log.d("userName", user.getUserName());
+        Log.d("Total Score", user.getScore().toString());
+
+        return view;
+    }
+}
